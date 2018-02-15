a968cccc9a9c07a93f666c6fbb3572e147a3df77
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-02-15 13:13:16 -0200

    commit de ajuste de nome de arquivo e local.

--- restore.R
+++ restore.R
@@ -6,8 +6,8 @@
 
 #Comentar ou descomentar de acordo com a pasta raiz do Insync Local
 #F:\Documentos\Luciano\Insync
-load("F:/Documentos/Luciano/Insync/installed_packages.rda")
+#load("F:/Documentos/Luciano/Insync/installed_packages.rda")
 #load("/home/luciano/Insync/installed_packages.rda")
-#load("D:/Users/luciano.silva/Insync/installed_packages.rda")
+load("D:/Users/luciano.silva/Insync/installed_packages-lts02.rda")
 
 for (count in 1:length(installedpackages)) install.packages(installedpackages[count])
