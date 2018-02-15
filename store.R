dbb0fb3d649203b9d55c518797cf65c99372ea84
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-02-15 19:36:39 -0200

    upd

--- store.R
+++ store.R
@@ -5,5 +5,5 @@
 tmp = installed.packages()
 
 installedpackages = as.vector(tmp[is.na(tmp[,"Priority"]), 1])
-save(installedpackages, file="D:/Users/luciano.silva/Insync/installed_packages.rda")
+save(installedpackages, file="D:/Users/luciano.silva/Insync/installed_packages-RAPD.rda")
 #save(installedpackages, file="F:/Documentos/Luciano/Insync/installed_packages-lts02.rda")
