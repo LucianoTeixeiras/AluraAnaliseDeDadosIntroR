c3368d43f415441d0dbd0761c6f01f2de734c0bc
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-02-18 20:11:13 -0300

    Commit final aula 02

--- AULA_02 - Criando Gráficos/aula_02.R
+++ AULA_02 - Criando Gráficos/aula_02.R
@@ -5,3 +5,25 @@ rm(list = ls())
 library(readr)
 duracao <- read_csv("projeto/duracao.csv")
 View(duracao)
+
+#Renomear Nome de Coluna
+
+install.packages("plyr")
+library(plyr)
+
+duracao<-rename(duracao, replace = c("user_id"="aluno", "course_id"="curso","timeToFinish"="dias"))
+View(duracao)
+
+summary(duracao)
+
+plot(duracao$dias)
+
+hist(duracao$dias, breaks = 8)
+
+# Elementos Estéicos do Gráfico
+
+pdf("Histograma.pdf")
+
+hist(duracao$dias, breaks = 6, main = "Histograma de Tempo/Curos", ylab = "Quantidade", xlab = "Tempo", ylim = c(0,2500), col = "purple")
+
+dev.off()
