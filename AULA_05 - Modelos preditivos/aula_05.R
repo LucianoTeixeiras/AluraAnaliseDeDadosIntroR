376c1236d016f6ba4c5ccebdebe2b1fa85c9ee27
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-02-20 12:27:17 -0300

    Commit final do curso - Aula 05

--- AULA_05 - Modelos preditivos/aula_05.R
+++ AULA_05 - Modelos preditivos/aula_05.R
@@ -8,3 +8,30 @@ View(modelo.linear)
 
 abline(lm(popularidade_duracao$popularidade~popularidade_duracao$dias))
 
+#Modo Não Linear
+
+scatter.smooth(popularidade_duracao$dias, popularidade_duracao$popularidade)
+
+#Muda o caracter de identificação da informação no gráfico
+
+help('pch')
+
+scatter.smooth(popularidade_duracao$dias, popularidade_duracao$popularidade,pch = 16)
+
+scatter.smooth(popularidade_duracao$dias, popularidade_duracao$popularidade,pch = '.')
+
+scatter.smooth(popularidade_duracao$dias, popularidade_duracao$popularidade,pch = 21,
+               col = 'blue')
+
+# Enbelezando o Gráfico
+
+library(ggplot2)
+
+grafico <- ggplot(popularidade_duracao, aes(dias, popularidade))
+
+grafico <- grafico + geom_point()
+
+grafico <- grafico + geom_smooth()
+
+grafico
+
